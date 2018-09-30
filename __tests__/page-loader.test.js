c8eb117f4036a744afe2e6f674bd991d4a4a7fc3
Pavel Cherepanov <pavel@cherepanov.pro>
2018-09-30 04:42:30 +0300

    tests (page-loader) add test for load page with assets

--- __tests__/page-loader.test.js
+++ __tests__/page-loader.test.js
@@ -1,58 +1,115 @@
 import nock from 'nock';
 import os from 'os';
-import { promises } from 'fs';
-import { join as pathJoin, sep } from 'path';
+import { promises, constants } from 'fs';
+import path from 'path';
 import { resolve as resolveUrl } from 'url';
 import load from '../src';
 
+const exists = async p => promises.access(p, constants.R_OK).then(() => true).catch(() => false);
+
+const encoding = 'utf-8';
+const fixturesDir = '__tests__/__fixtures__';
+
+const getFixturePath = name => path.join(fixturesDir, name);
+
+const assets = [
+  'img.png',
+  'script.old.js',
+  'css/custom.css',
+];
+
+const expectedAssetsFiles = [
+  'img.png',
+  'script-old.js',
+  'css-custom.css',
+];
+
+const host = 'http://ru.hexlet.io';
+
+const simplePageUri = '/simple';
+const simpleHtml = '<html><head></head><body><div id="test"></div></body></html>';
+const simpleSourceUri = resolveUrl(host, simplePageUri);
+const expectedSimpleFilename = 'ru-hexlet-io-simple.html';
+
+const fixtureFilename = 'courses.html';
+const expectedFixtureFilename = 'expected.html';
+const complexPageUri = '/courses/1';
+const complexSourceUri = resolveUrl(host, complexPageUri);
+const expectedComplexPageFilename = 'ru-hexlet-io-courses-1.html';
+const expectedIncludeDirname = 'ru-hexlet-io-courses-1_files';
+
 describe('Page Loader', () => {
-  const host = 'http://ru.hexlet.io';
-  const pageUri = '/courses';
-  const sourceUri = resolveUrl(host, pageUri);
-  const expectedFilename = 'ru-hexlet-io-courses.html';
-  const data = '<html><head></head><body>test data</body></html>';
+  let currentTempDir = null;
 
   beforeAll(() => {
     nock.disableNetConnect();
-  });
 
-  it('Load simple page', async () => {
     nock(host)
-      .get(pageUri)
-      .reply(200, data);
+      .get(simplePageUri)
+      .reply(200, simpleHtml);
 
-    const tempDir = await promises.mkdtemp(pathJoin(os.tmpdir(), sep), 'utf8');
-    const expectedPath = pathJoin(tempDir, expectedFilename);
+    nock(host)
+      .get(complexPageUri)
+      .replyWithFile(200, getFixturePath(fixtureFilename));
 
-    const storedName = await load(sourceUri, tempDir);
-    expect(storedName).toBe(expectedPath);
-
-    const storedData = await promises.readFile(storedName, 'utf8');
-    expect(storedData).toBe(data);
-  });
-
-  it('Load broken uri', async () => {
     nock(host)
       .get('/wrong')
       .reply(404);
 
+    nock(host)
+      .get('/courses/assets/wrong')
+      .reply(404);
+
+    assets.forEach(asset => nock(host)
+      .get(resolveUrl(complexPageUri, asset))
+      .replyWithFile(200, getFixturePath(asset)));
+  });
+
+  beforeEach(async () => {
+    currentTempDir = await promises.mkdtemp(path.join(os.tmpdir(), path.sep), 'utf8');
+  });
+
+  it('Load simple page without links', async () => {
+    const storedName = await load(simpleSourceUri, currentTempDir);
+    const expectedPath = path.join(currentTempDir, expectedSimpleFilename);
+    expect(storedName).toBe(expectedPath);
+
+    const storedData = await promises.readFile(storedName, 'utf8');
+    expect(storedData).toBe(simpleHtml);
+  });
+
+  it('Load broken uri', async () => {
     try {
-      const tempDir = await promises.mkdtemp(pathJoin(os.tmpdir(), sep), 'utf8');
-      await load(resolveUrl(host, '/wrong'), tempDir);
+      await load(resolveUrl(host, '/wrong'), currentTempDir);
     } catch (e) {
       expect(e.response.status).toBe(404);
     }
   });
 
   it('Load simple page to no entity directory', async () => {
-    nock(host)
-      .get('/')
-      .reply(200, data);
-
     try {
-      await load(sourceUri, pathJoin(os.tmpdir(), '/wrong'));
+      await load(simpleSourceUri, path.join(os.tmpdir(), '/wrong'));
     } catch (e) {
       expect(e.code).toBe('ENOENT');
     }
+  });
+
+  // FIXME: нужно сделать проверку на вложенные локальные ресурсы
+  it('Load page with local, extend and not existed assets', async () => {
+    const storedName = await load(complexSourceUri, currentTempDir);
+    const expectedPath = path.join(currentTempDir, expectedComplexPageFilename);
+    expect(storedName).toBe(expectedPath);
+    expect(await exists(storedName)).toBe(true);
+    const storedHtml = await promises.readFile(storedName, encoding);
+
+    const expectedHtml = await promises.readFile(getFixturePath(expectedFixtureFilename), encoding);
+    expect(storedHtml).toEqual(expectedHtml);
+
+    const expectedDirPath = path.join(currentTempDir, expectedIncludeDirname);
+    expect(await exists(expectedDirPath)).toBe(true);
+
+    const storedAssets = await promises.readdir(expectedDirPath, encoding);
+
+    expect(storedAssets.sort()).toEqual(expectedAssetsFiles.sort());
   });
 });
